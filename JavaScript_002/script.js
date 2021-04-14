b2519d63b8b62e4770e3d05ff361494cc279d1b5
Tommy Veen <75741894+tommyveen@users.noreply.github.com>
2021-04-14 18:13:49 +0200

    Add files via upload

---
+++ JavaScript_002/script.js
@@ -0,0 +1,6 @@
+console.log("Hallo Winc Academy studenten")
+// let multiply = 1230941 * 1823794;
+// console.log(multiply)
+// let divide = 637263 / 54;
+// console.log(divide)
+console.log("Dit is een grote som:")
